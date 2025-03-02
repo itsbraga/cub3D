425a17e5fbdcdb820c5389ddbae8d94608b95ab7
Mateo PRIOUX <pmateo@bess-f2r4s12.clusters.42paris.fr>
2025-03-02 18:16:16 +0100

    merge

--- mandatory/render/generate_img_by_xpm.c
+++ mandatory/render/generate_img_by_xpm.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   generate_img.c                                     :+:      :+:    :+:   */
+/*   generate_img_by_xpm.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 19:33:51 by art3mis           #+#    #+#             */
-/*   Updated: 2025/03/02 14:46:45 by pmateo           ###   ########.fr       */
+/*   Updated: 2025/03/02 17:51:17 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D_bonus.h"
+#include "cub3D.h"
 
 t_img	generate_img(char *relative_path)
 {
@@ -32,7 +32,7 @@ t_img	generate_img(char *relative_path)
 	if (img.addr == NULL)
 	{
 		err_msg("MinilibX", ERR_ADDR, 0);
-		del_img(mlx_s(), img.img_ptr);
+		del_img(mlx_s());
 		ft_memset(&img, 0, sizeof(t_img));
 	}
 	return (img);
